79001149283e4f6ef8e142baaff4b126f9d4a7d6
Ji ZHANG <zhangji87@gmail.com>
2022-01-24 16:53:16 +0800

    Tidy up for blog demos

--- misc/src/main/java/com/shzhangji/javasandbox/di/App.java
+++
@@ -1,7 +0,0 @@
-package com.shzhangji.javasandbox.di;
-
-public class App {
-  public static void main(String[] args) {
-    ServiceLocator.metaService().greeting();
-  }
-}
--- misc/src/main/java/com/shzhangji/javasandbox/di/MetaService.java
+++
@@ -1,7 +0,0 @@
-package com.shzhangji.javasandbox.di;
-
-public class MetaService {
-  public void greeting() {
-    System.out.println("hello world");
-  }
-}
