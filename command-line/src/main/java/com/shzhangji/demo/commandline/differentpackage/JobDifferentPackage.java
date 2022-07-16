432941f19cca414a5d69cfdd19fc166699771b90
Ji ZHANG <zhangji87@gmail.com>
2022-07-16 18:18:36 +0800

    Remove JobDifferentMain

--- command-line/src/main/java/com/shzhangji/demo/commandline/differentpackage/JobDifferentPackage.java
+++ command-line/src/main/java/com/shzhangji/demo/commandline/differentpackage/JobDifferentPackage.java
@@ -1,14 +1,28 @@
 package com.shzhangji.demo.commandline.differentpackage;
 
+import com.shzhangji.demo.commandline.common.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// @SpringBootApplication
+// @Import(UserService.class)
+@SpringBootApplication(scanBasePackages = {
+    "com.shzhangji.demo.commandline.common",
+    "com.shzhangji.demo.commandline.differentpackage",
+})
 @Slf4j
-@Component
 @RequiredArgsConstructor
 public class JobDifferentPackage implements CommandLineRunner {
+  private final UserService userService;
+
+  public static void main(String[] args) {
+    SpringApplication.run(JobDifferentPackage.class, args);
+  }
+
   @Override
   public void run(String... args) {
     log.info("Run JobDifferentPackage");
